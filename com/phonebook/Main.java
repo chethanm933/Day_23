3c74b2a5c2293c80dde46a519bea3f75952be6aa
chethanm933/chethanm933 <chethanm933@gmail.com>
2022-01-06 10:09:29 +0530

    Creating contact, Adding Contact details into phoneook, viewing contact and editing contact details

---
+++ com/phonebook/Main.java
@@ -0,0 +1,185 @@
+package com.phonebook;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	static List<Contacts> phoneBook;
+	static PhoneBook phonebook = new PhoneBook();
+	static Scanner sc = new Scanner(System.in);
+	static Contacts contact;
+	
+	public static void main(String[] args) {
+		
+		phoneBook = new ArrayList<Contacts>();
+		
+		
+		Main main = new Main();
+		boolean isExit = false;
+		
+		while(!isExit) {
+			if(phoneBook.isEmpty()) {
+				System.out.println("Select your option\\n1. Add Contact" + "\n5. Exit");
+		} else {
+			System.out.println("Select your option\n1.Add Contact\n2.Edit Contact\n3.Delete Contact\n4.View Contacts\n5.Exit");
+		}
+			int option = sc.nextInt();
+			switch(option) {
+			
+			case 1:
+				addContacts();
+				break;
+				
+			case 2:
+				main.editContact();
+				break;
+				
+			case 3:
+				main.removeContact();
+				break;
+				
+			case 4:
+				main.viewAllContact();
+				break;
+				
+			}
+		}
+	}
+	
+	public void editContact() {
+		System.out.println("Please enter a name which you want to edit :");
+		String name = sc.next();
+		Contacts editContact = null;
+		for (int i = 0; i < phoneBook.size(); i++) {
+			if(name.equals(phoneBook.get(i).getName())) {
+				editContact = phoneBook.get(i);
+				
+				String isExit = "n";
+				
+			do {
+				System.out.println("Please Enter which you want to edit :\n1.Name\n2.LastName\n3.Phone Number\n4.Address\5.City\n6.State\n7.Zip code\n8.Emailid\n9.Exit");
+				int option = sc.nextInt();
+				
+				switch(option) {
+				
+				case 1:
+					
+					System.out.println("Please enter a New name:");
+					String newName = sc.next();
+					editContact.setName(newName);
+					break;
+				
+				
+				case 2:
+					System.out.println("Please enter a Last name:");
+					String newLastName = sc.next();
+					editContact.setLastName(newLastName);
+					break;
+				
+			
+				case 3:
+					System.out.println("Please enter a Phone Number:");
+					String newPhone = sc.next();
+					editContact.setPhone(newPhone);
+					break;
+					
+				case 4:
+					System.out.println("Please enter a Address:");
+					String newAddress = sc.next();
+					editContact.setAddress(newAddress);
+					break;
+					
+				case 5:
+					System.out.println("Please enter a City:");
+					String newCity = sc.next();
+					editContact.setCity(newCity);
+					break;
+					
+		
+				case 6:
+					System.out.println("Please enter a New State:");
+					String newState = sc.next();
+					editContact.setState(newState);
+					break;
+					
+				case 7:
+					System.out.println("Please enter a zip:");
+					String newZip = sc.next();
+					editContact.setZip(newZip);
+					break;
+					
+				case 8:
+					System.out.println("Please enter a zip:");
+					String newEmail = sc.next();
+					editContact.setZip(newEmail);
+					break;
+					
+				case 9:
+					System.out.println("Do you wnat to exit :y/n");
+					isExit = sc.next();
+					break;
+					
+				default:
+					System.out.println("invalid option");
+					break;
+				}
+			} while(isExit.equalsIgnoreCase("y"));
+		} else {
+			System.out.println("No details found with this Name :");
+			}
+		}
+	}
+	
+	public void viewAllContact() {
+		
+		if(phoneBook.isEmpty()) {
+			System.out.println("Your PhoneBook is Empty");
+		} else {
+			for (Contacts view : phoneBook) {
+				System.out.println(view);
+			}
+		}
+	}
+	
+	public void exit() {
+		System.out.println("Do you want to exit :");
+		
+	}
+	
+		
+		
+	public void removeContact() {
+		System.out.println("Please enter a name which you want to remove :");
+		String removeName = sc.next();
+		Contacts removeContact = null;
+		for (int i = 0; i < phoneBook.size(); i++) {
+			if(phoneBook.get(i).equals(removeName)) {
+				removeContact = phoneBook.remove(i);
+				System.out.println("The Contact has successfully deleted :");
+			} else {
+				System.out.println("No Contact found with this Name :");
+				}
+		}
+	}
+	
+	
+	public static void addContacts() {
+		Contacts contact = new Contacts();
+		contact.setName(phonebook.getName());
+		contact.setLastName(phonebook.getLastName());
+		contact.setPhone(phonebook.getPhone());
+		contact.setAddress(phonebook.getAddress());
+		contact.setCity(phonebook.getCity());
+		contact.setState(phonebook.getState());
+		contact.setZip(phonebook.getZip());
+		contact.setEmail(phonebook.getEmail());
+		
+		phoneBook.add(contact);
+		System.out.println("your contact saved successfully :\n thank you");
+		
+		}
+		
+	}
+
---
+++ com/phonebook/PhoneBook.java
@@ -0,0 +1,139 @@
+package com.phonebook;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneBook {
+	
+	static Scanner sc = new Scanner(System.in);
+	Contacts contact = new Contacts(); 
+	
+	public String getName() {
+		System.out.println("Enter a First Name");
+		String name = sc.next();
+		String namePattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileName = Pattern.compile(namePattern);
+		Matcher nameMatcher = compileName.matcher(name);
+		
+		while(!nameMatcher.matches()) {
+			System.out.println("You Entered an Invalid Name Please Enter it Again");
+			name = sc.next();
+			nameMatcher = compileName.matcher(name);
+		}
+		return name;
+	}
+	
+	public String getLastName() {
+		System.out.println("Enter a Last Name");
+		String lastName = sc.next();
+		String lastNamePattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileLastName = Pattern.compile(lastNamePattern);
+		Matcher lastNameMatcher = compileLastName.matcher(lastName);
+		
+		while(!lastNameMatcher.matches()) {
+			System.out.println("You Entered an Invalid Last Name Please Enter it Again");
+			lastName = sc.next();
+			lastNameMatcher = compileLastName.matcher(lastName);
+		}
+		return lastName;
+	}
+	
+	
+	public String getPhone() {
+		System.out.println("Enter a Phone Number");
+		String phone = sc.next();
+		String phonePattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compilePhone = Pattern.compile(phonePattern);
+		Matcher phoneMatcher = compilePhone.matcher(phone);
+		
+		while(!phoneMatcher.matches()) {
+			System.out.println("You Entered an Invalid Phone Number Please Enter it Again");
+			phone = sc.next();
+			phoneMatcher = compilePhone.matcher(phone);
+		}
+		return phone;
+	}
+	
+	public String getAddress() {
+		System.out.println("Enter a Address");
+		String address = sc.next();
+		String addressPattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileAddress = Pattern.compile(addressPattern);
+		Matcher addressMatcher = compileAddress.matcher(address);
+		
+		while(!addressMatcher.matches()) {
+			System.out.println("You Entered an Invalid Address Please Enter it Again");
+			address = sc.next();
+			addressMatcher = compileAddress.matcher(address);
+		}
+		return address;
+	}
+	
+	public String getCity() {
+		System.out.println("Enter a City");
+		String city = sc.next();
+		String cityPattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileCity = Pattern.compile(cityPattern);
+		Matcher cityMatcher = compileCity.matcher(city);
+		
+		while(!cityMatcher.matches()) {
+			System.out.println("You Entered an Invalid City Please Enter it Again");
+			city = sc.next();
+			cityMatcher = compileCity.matcher(city);
+		}
+		return city;
+	}
+	
+	public String getZip() {
+		System.out.println("Enter a Zip Code");
+		String zip = sc.next();
+		String zipPattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileZip = Pattern.compile(zipPattern);
+		Matcher zipMatcher = compileZip.matcher(zip);
+		
+		while(!zipMatcher.matches()) {
+			System.out.println("You Entered an Invalid Zip Code Please Enter it Again");
+			zip = sc.next();
+			zipMatcher = compileZip.matcher(zip);
+		}
+		return zip;
+	}
+	
+	public String getEmail() {
+		System.out.println("Enter a Email-id");
+		String email = sc.next();
+		String emailPattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileEmail = Pattern.compile(emailPattern);
+		Matcher emailMatcher = compileEmail.matcher(email);
+		
+		while(!emailMatcher.matches()) {
+			System.out.println("You Entered an Invalid Email-id Please Enter it Again");
+			email = sc.next();
+			emailMatcher = compileEmail.matcher(email);
+		}
+		return email;
+	}
+	
+	public String getState() {
+		System.out.println("Enter a State");
+		String state = sc.next();
+		String statePattern = "^[A-Z]{1}[a-z]{2,10}$";
+		Pattern compileState = Pattern.compile(statePattern);
+		Matcher stateMatcher = compileState.matcher(state);
+		
+		while(!stateMatcher.matches()) {
+			System.out.println("You Entered an Invalid State Please Enter it Again");
+			state = sc.next();
+			stateMatcher = compileState.matcher(state);
+		}
+		return state;
+	}
+	
+	
+	
+}
+	
+
+
